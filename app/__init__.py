98d874d278dde8b3f7b61f6daa8b64b66c6f2621
Aaron Bruce <aaronbruce555@gmail.com>
2020-08-11 16:42:29 -0400

    Add new ERD and Add files for docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,17 @@
 from flask import Flask
 from app.config import Configuration
 from app.models.models import db
+from flask_cors import CORS
+from flask_restx import Api
+from app.routes.auth import api as auth
+# import app.routes.<<file>>
 
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
+
+jwt = JWTManager(app)
+app.config.from_object(Configuration)
+db.init_app(app)
+
+api = Api
