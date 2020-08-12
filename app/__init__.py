97b50db3873556ad288467fcb42b8cdd05705ea0
Aaron Bruce <aaronbruce555@gmail.com>
2020-08-12 12:06:03 -0400

    Add database.py seed file and update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restx import Api
 from app.routes.auth import api as auth
-# from app.routes.users import api as users
+from app.routes.user import api as user
+from app.routes.crypto import api as crypto
+from app.routes.watch_list_item import api as watch_list_item
 
 
 app = Flask(__name__)
@@ -18,5 +20,7 @@ db.init_app(app)
 
 api = Api(app)
 api.add_namespace(auth)
-# api.add_namespace(users)
+api.add_namespace(user)
+api.add_namespace(crypto)
+api.add_namespace(watch_list_item)
 Migrate(app, db)
---
+++ database.py
@@ -0,0 +1,61 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from app import app, db
+from app.models.models import (
+  User, Crypto, Watch_List_Item
+)
+
+with app.app_context():
+  db.drop_all()
+  db.create_all()
+
+  # 1 user
+  # 5 crypto
+  # 3 watch_list
+
+  user1 = User(
+    first_name='Aaron',
+    last_name='Bruce',
+    email='aaronbruce555@gmail.com',
+    phone_number='2035251846',
+    hashed_password='password',
+  )
+
+  crypto1 = Crypto(
+    name='Bitcoin',
+    symbol='BTC',
+  )
+
+  crypto2 = Crypto(
+    name='Etherium',
+    symbol='ETH'
+  )
+
+  crypto3 = Crypto(
+    name='LiteCoin',
+    symbol='LTC'
+  )
+
+  crypto4 = Crypto(
+    name='Bitcoind Cash',
+    symbol='BCH'
+  )
+
+  crypto5 = Crypto(
+    name='Ripple',
+    symbol='XRP'
+  )
+
+  watch_item1 = Watch_List_Item(
+    user_id=1
+    crypto_id=1
+  )
+  watch_item2 = Watch_List_Item(
+    user_id=1
+    crypto_id=2
+  )
+  watch_item3 = Watch_List_Item(
+    user_id=1
+    crypto_id=3
+  )
